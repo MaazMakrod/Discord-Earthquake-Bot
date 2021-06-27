9860c7e78643507a55e4b43fc23dad1448caf292
MaazMakrod <67477587+MaazMakrod@users.noreply.github.com>
2021-06-27 12:55:41 -0400

    Formatting Edits

--- main.py
+++ main.py
@@ -42,27 +42,28 @@ async def on_message(message):
         if depth == "n/a":
             depth = "_unavailable_"
 
-        response = "Did someone call to hear about an earthquake?\n\nThe most recent earthquake occured on **%s** at**%s**. The magnitude of this earthquake was **%s** on the Richter scale and its depth was **%s**.\n\n_All information is from_ `https://www.volcanodiscovery.com/earthquakes/today.html`" % (
-            time, location, mag, depth)
+        response = "Did someone call to hear about an earthquake?\n\nThe most recent earthquake occured on **%s** at**%s**. The magnitude of this earthquake was **%s** on the Richter scale and its depth was **%s**.\n\n_All information is from_ `https://www.volcanodiscovery.com/earthquakes/today.html`" % (time, location, mag, depth)
 
         await message.channel.send(response)
+        
     elif message.content.startswith('$dailysummary'):
-        summary = soup.find_all("div", attrs={"class":
-                                              "textbox"})[0].get_text()
+        summary = soup.find_all("div", attrs={"class":"textbox"})[0].get_text()
         index = summary.find("writeAge")
         paragraph = summary[0:index]
         index = paragraph.find("Biggest")
         topLines = paragraph[0:index]
+        
         response = "%s \n\n_All information is from_ `https://www.volcanodiscovery.com/earthquakes/today.html`" % topLines
         await message.channel.send(response)
+        
     elif message.content.startswith('$strongestquake'):
         text = soup.find('div', attrs={'class': 'textbox'}).getText()
         start = text.find('Biggest quake')
         end = text.find('Most recent quake')
-        response = "%s \n\n_All information is from_ `https://www.volcanodiscovery.com/earthquakes/today.html`" % text[
-            start:end]
-
+        
+        response = "%s \n\n_All information is from_ `https://www.volcanodiscovery.com/earthquakes/today.html`" % text[start:end]
         await message.channel.send(response)
+        
     elif message.content.startswith('$help'):
         response = "Commands:\n\n`$earthquake` to see the most recent earthquake\n\n`$dailysummary` to see the daily summary of all the earthquakes across the globe\n\n`$strongestquake` to see the strongest earthquake today"
 
